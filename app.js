928680eda9ef356aa117f77f841cfe95a7ea54ac
root <root@Arthur.localdomain>
2018-09-15 00:04:15 -0700

    aboutme file updated

---
+++ app.js
@@ -0,0 +1,18 @@
+var name =prompt('what is my name?');
+alert('Hello ' + name);
+var haveLived =prompt('where have i lived?');
+alert('right on ' + name + ' please continue and tell me more about ' + haveLived); 
+var favFood =prompt('what is my favourite food?');
+alert('you seem so sure ' + favFood );
+var sport =prompt('what sport have i played?');
+var location =prompt('if so where?');
+
+//response:('Hello '+ name + ' you live in' + haveLived + 'and your favourite food is' + ' favFood');
+var response="Hello " + name + " you live in " + haveLived + " and your favourite food is " + favFood + " sports " + sport + " location " + location
+
+console.log('userName' + name.toUpperCase());
+console.log('you have lived' + haveLived.toUpperCase());
+alert(response)
+console.log('Favourite' + favFood.toUpperCase());
+console.log('Sport played' + sport.toUpperCase());
+console.log('Location of the sport' + location.toUpperCase());
